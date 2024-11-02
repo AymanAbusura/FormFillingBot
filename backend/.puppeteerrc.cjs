a0a85d4416c4addb5f2b581a0090de689b4ad652
Ayman Abusura <aymansalameh1996@hotmail.com>
2024-11-02 10:41:06 +0300

    Done

--- backend/.puppeteerrc.cjs
+++ backend/.puppeteerrc.cjs
@@ -1,23 +1,8 @@
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-    // Download Chrome (default `skipDownload: false`).
-    chrome: {
-      skipDownload: false,
-    },
-    // Download Firefox (default `skipDownload: true`).
-    firefox: {
-      skipDownload: false,
-    },
-  };
-
-  const {join} = require('path');
+const {join} = require('path');
 
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
