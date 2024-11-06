f4342b1187c8c47e0e8fdabb587261f9dd9f81db
Ayman Abusura <aymansalameh1996@hotmail.com>
2024-11-06 18:00:15 +0300

    Done

--- backend/server.js
+++ backend/server.js
@@ -26,11 +26,11 @@ app.use(cors({
     maxAge: 3600,
 }));
 // Add CORS headers to all responses
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 app.options('*', cors());
 
