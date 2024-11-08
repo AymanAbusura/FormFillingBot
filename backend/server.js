9b71dcbada0a557a6e8936f22eccb2bfc0eea8b4
Ayman Abusura <aymansalameh1996@hotmail.com>
2024-11-08 14:45:47 +0300

    Done

--- backend/server.js
+++ backend/server.js
@@ -62,6 +62,7 @@ app.post('/fill-form-without-proxy', async (req, res) => {
     try {
         const browserOptions = {
             headless: true,
+            executablePath: '/opt/render/.cache/puppeteer/chrome/linux-130.0.6723.116/chrome-linux64/chrome', // Explicit Chrome path
             // headless: false,
             args: [
                 "--disable-setuid-sandbox",
@@ -147,6 +148,7 @@ app.post('/fill-form-with-proxy', async (req, res) => {
     try {
         const browserOptions = {
             headless: true,
+            executablePath: '/opt/render/.cache/puppeteer/chrome/linux-130.0.6723.116/chrome-linux64/chrome', // Explicit Chrome path
             // headless: false,
             args: [
                 "--disable-setuid-sandbox",
